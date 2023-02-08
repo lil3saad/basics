7139474d5b3d5f19c2a9c0bbda404550fbb0d1f0
syed saad <saadinbusiness3@gmail.com>
2023-02-08 20:11:51 +0530

    made changes to palindrom

--- Palindrome/src/Main.java
+++ Palindrome/src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+        int n = 1521;
+        int og=151;
+        int rev=0;
+        while (n!=0){
+            rev = rev * 10 + n%10;
+            n=n/10;
+        }
+        System.out.println("The rev number = "+rev);
+        if(og==rev){
+            System.out.println("It is a palindrome");
+        }
+        else System.out.println("It is not a palindrome");
     }
 }
